a939f058a361d807d8f5b5272da62fbb815ac336
Bruno Ferreira <fs.brunoferreira@gmail.com>
2022-01-15 15:58:37 -0300

    test: add create user

---
+++ tests/Feature/UsersControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UsersControllerTest extends TestCase
+{
+    use WithFaker;
+
+    public function test_create_user()
+    {
+        $payload = [
+            'name' => 'any_create_content',
+            'username' => 'any123'
+        ];
+        $this->json('POST', '/api/v1/users', $payload)
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'name', 'username', 'created_at', 'updated_at', 'id'
+            ]);
+    }
+
+    public function test_validation_create_user()
+    {
+        $payload = [
+            'name' => 'Any',
+            'username' => 'test!@#%4'
+        ];
+        $this->json('POST', '/api/v1/users', $payload)
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'username' => ['The username must only contain letters and numbers.']
+                ]
+            ]);
+
+        $payload = [
+            'name' => 'Any',
+            'username' => 'username123456123'
+        ];
+        $this->json('POST', '/api/v1/users', $payload)
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'username' => ['The username must not be greater than 14 characters.']
+                ]
+            ]);
+    }
+}
